90750da98a6f926feee9f6d3421238e6f712b2ab
Divjot bedi <divjotbedi16706@gmail.com>
2026-05-26 22:29:29 +0530

    Implement even/odd number checker in if-else.cpp

--- if-else.cpp
+++ if-else.cpp
@@ -1,1 +1,14 @@
-//start
+#include <streamio>
+
+int main(){
+  int x;
+  std::cout << "Enter Number:";
+  std::cin >> x;
+  if(x%2==0){
+    std::cout << "Even Number\n";
+  }
+  else{
+    std::cout << "Odd Number\n";
+  }
+  return 0;
+}
